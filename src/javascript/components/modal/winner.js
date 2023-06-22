c350ed4090340712b0e65ca4c34bf87254959485
Sakhno Andrii <andreysakhno1986@gmail.com>
2023-06-22 17:11:21 +0300

    * Modified winner modal

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,8 +1,11 @@
 import showModal from './modal';
+import { createFighterImage } from '../fighterPreview';
 
 export default function showWinnerModal(fighter) {
-    const { name: title, fighterElement } = fighter;
-    const bodyElement = fighterElement.firstElementChild.cloneNode(true);
+    const { name: title } = fighter;
+
+    const bodyElement = createFighterImage(fighter);
+
     const onClose = () => {
         window.location.reload();
     };
