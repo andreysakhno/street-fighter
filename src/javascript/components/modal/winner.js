53928f8103f00435a624a3b0eedce008139a35b1
Sakhno Andrii <andreysakhno1986@gmail.com>
2023-06-22 20:26:28 +0300

    * Modified winner modal

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,10 +1,28 @@
 import showModal from './modal';
-import { createFighterImage } from '../fighterPreview';
+import createElement from '../../helpers/domHelper';
+
+function createImage(fighter) {
+    const { source, name } = fighter;
+    const attributes = {
+        src: source,
+        title: name,
+        alt: name
+    };
+    const imgElement = createElement({
+        tagName: 'img',
+        className: 'modal-fighter-image',
+        attributes
+    });
+
+    return imgElement;
+}
 
 export default function showWinnerModal(fighter) {
     const { name: title } = fighter;
 
-    const bodyElement = createFighterImage(fighter);
+    const bodyElement = createElement({ tagName: 'div', className: 'modal-body' });
+    const imageElement = createImage(fighter);
+    bodyElement.append(imageElement);
 
     const onClose = () => {
         window.location.reload();
